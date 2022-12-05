a035e2ba644897cc87bc9cc35b11c3d85642c842
skobtax <admin@deco.tax>
2022-12-05 14:48:14 -0500

    Add CSS file.

--- webpkg/webpack.config.js
+++ webpkg/webpack.config.js
@@ -1,12 +1,27 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+
 module.exports = {
   entry: './bootstrap.js',
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [ MiniCssExtractPlugin.loader, 'css-loader' ]
+      }
+    ]
+  },
+  optimization: {
+    minimizer: [ `...`, new CssMinimizerPlugin() ]
+  },
   plugins: [
     new HtmlWebpackPlugin({
       title: 'DecoTax',
       filename: 'main.html',
       template: 'main-template.html'
-    })
+    }),
+    new MiniCssExtractPlugin()
   ]
 };
