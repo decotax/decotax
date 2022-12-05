24c01191df52decc1ed4e6b667655a2382963274
skobtax <admin@deco.tax>
2022-12-05 14:38:24 -0500

    Add HTML file saying "under construction".

--- webpkg/webpack.config.js
+++ webpkg/webpack.config.js
@@ -1,3 +1,12 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
-  entry: './bootstrap.js'
+  entry: './bootstrap.js',
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'DecoTax',
+      filename: 'main.html',
+      template: 'main-template.html'
+    })
+  ]
 };
