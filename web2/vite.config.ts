365f2f67ae8cfd4bc39343a02c6635d5e7a696f3
skobtax <admin@deco.tax>
2025-07-29 23:14:29 -0400

    Minify HTML.

---
+++ web2/vite.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import { createHtmlPlugin } from "vite-plugin-html";
+
+export default defineConfig({
+  plugins: [
+    vue(),
+    createHtmlPlugin()
+  ]
+});
