8353a32b7ae0f1545eb8916fbdcc31b7993eae75
skobtax <admin@deco.tax>
2025-08-03 15:56:24 -0400

    Silly template idea.

--- web2/vite.config.ts
+++ web2/vite.config.ts
@@ -1,18 +1,25 @@
-import { defineConfig } from "vite";
+import { defineConfig, Plugin } from "vite";
 import vue from "@vitejs/plugin-vue";
 import { minify } from "html-minifier-terser";
+import fs from "fs";
 
-const MinifyHtmlPlugin = () => {
+export default defineConfig({
+  plugins: [ PageTemplatePlugin(), vue(), MinifyHtmlPlugin() ]
+});
+
+function PageTemplatePlugin(): Plugin {
+  const marker = "<!-- $CONTENT -->";
+  const template = fs.readFileSync("page-template.html", "utf-8");
+  const handler = (html: string) => template.replace(marker, html);
+  return { name: "vite-plugin-page-template",
+           transformIndexHtml: { order: "pre", handler } };
+}
+
+function MinifyHtmlPlugin(): Plugin {
   const handler = (html: string) => minify(html, {
     collapseWhitespace: true,
     removeComments: true
   });
-  return {
-    name: "minify-html",
-    transformIndexHtml: { handler }
-  };
-};
-
-export default defineConfig({
-  plugins: [ vue(), MinifyHtmlPlugin() ]
-});
+  return { name: "vite-plugin-minify-html",
+           transformIndexHtml: { handler } };
+}
