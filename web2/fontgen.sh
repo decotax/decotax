253a463a60d31382084c66814de43a754c21f135
Steve Kobes <steve@kobes.ca>
2026-01-19 23:59:24 -0500

    Process bold and italic.

--- web2/fontgen.sh
+++ web2/fontgen.sh
@@ -1,21 +1,36 @@
 #!/bin/bash
 
 fonttools() {
-  uvx --with brotli --from 'fonttools[woff]@latest' \
-    -- fonttools "$@"
+  uvx --with brotli --from 'fonttools[woff]@latest' fonttools "$@"
 }
 mkdir -p gen
 
-SRCDIR="./node_modules/@fontsource-variable/source-sans-3/files"
-SRC="${SRCDIR}/source-sans-3-latin-wght-normal.woff2"
+process() {
+  PKG=$1
+  SRCBASE=$2
+  DSTBASE=$3
+  WGHT=$4
 
-URANGE='00-7F,A0,AD,200B,2011,FEFF,FFFD'
-EXTRA='§¶½×÷–—‘’“”…∕éàèùçâêîôûëïüÉÀÈÙÇÂÊÎÔÛËÏÜ'
+  SRCDIR="./node_modules/@fontsource-variable/${PKG}/files"
+  SRC="${SRCDIR}/${SRCBASE}.woff2"
+  INT="gen/${DSTBASE}-sub.woff2"
+  DST="gen/${DSTBASE}.woff2"
 
-fonttools subset $SRC --flavor=woff2 --output-file=gen/ss3sub.woff2 \
-  --unicodes="$URANGE" --text="$EXTRA"
-fonttools varLib.instancer -o gen/ss3.woff2 gen/ss3sub.woff2 wght=400
-rm gen/ss3sub.woff2
+  URANGE='00-7F,A0,AD,200B,2011,FEFF,FFFD'
+  EXTRA='§¶½×÷–—‘’“”…∕éàèùçâêîôûëïüÉÀÈÙÇÂÊÎÔÛËÏÜ'
 
-wc -c $SRC
-wc -c gen/ss3.woff2
+  fonttools subset $SRC --flavor=woff2 --output-file=${INT} \
+    --unicodes="$URANGE" --text="$EXTRA"
+  fonttools varLib.instancer -o ${DST} -q ${INT} wght=${WGHT}
+  rm ${INT}
+
+  SZ1=$(wc -c < ${SRC})
+  SZ2=$(wc -c < ${DST})
+  RED=$(printf "%.1f" "$(echo "scale = 4; 100 - ($SZ2 / $SZ1) * 100" | bc)")
+  echo "${SRCBASE} $SZ1 -> ${DSTBASE} $SZ2 -${RED}%"
+}
+
+process "source-sans-3" "source-sans-3-latin-wght-normal" "ss3" 400
+process "source-sans-3" "source-sans-3-latin-wght-italic" "ss3i" 400
+process "source-sans-3" "source-sans-3-latin-wght-normal" "ss3b" 700
+process "source-sans-3" "source-sans-3-latin-wght-italic" "ss3bi" 700
